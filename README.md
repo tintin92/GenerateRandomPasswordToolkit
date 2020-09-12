68aba95982636f6f9ddfb60acbbe902b60f577d6
Tintin <tanner@schenck.xyz>
2020-09-11 20:39:28 -0400

    added more psuedo code to index file, created variables for js file

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ alert Password length must be at least 8 characters
 
 if, else-if, or else (keyword), a condition, and the resulting code in { } curly brackets 
 
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- script.js
+++ script.js
@@ -1,45 +1,48 @@
 // Assignment Code
-                      //selecting document from the dom 
 var generateBtn = document.querySelector("#generate");
-// Special Characters for the function created 
-const specialCharacters = "!@#$%^&*()";
+
+//Define variables to help organize 
+var specialChar;
+var numberAr;
+var upperAr;
+var lowerAr;
 
 
-  // Write password to the #password input
+//user input variables:
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var numberAr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var upperAr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerAr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+console.log(specialChar)
+//create functions to write variables 
 function writePassword() {
+
+   //length of password
+  user = prompt (" to begin please enter a number between 8 and 128.");
+//character requirement 
+
+  var passwordLength = prompt()
+  var numbers = confirm("Do you want numbers in your password?");
+  var lowerCases = confirm("Do you want lowercases in your password ")
+  var upperCases = confirm("Do you want uppercases in your password")
+  var special = confirm("Do you want special characters in your password?");
+  var minimumCount = 0;
+
+
+
+  return testAlert
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-
+  passwordText.value = password;
 
 }
 
+// arrays to hold characters that will make up random password
+// Array of numbers user can use if wanting numbers 
 
 
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//Prompts that come up after you click generate password//
-function generatePassword() {
-  var passwordLength = prompt("Please enter the number of characters you want for your new password. It must be at least 8 characters and no more than 128.");
-    while (passwordLength < 7 || passwordLength < 128) {
-      alert("Sorry, the password length be between 8 and 128 characters.");
-      //re-issue Alert to insert correct character length for password
-      var passwordLength = prompt("Please enter the number of characters you want for your new password. It must be at least 8 characters and no more than 128.");
-    }
-    if (passwordLength > 7 || passwordLength < 129) {
-
-
-  var passwordLength = prompt ()
-  var numbers = confirm("Do you want numbers in your password?");
-  var lowerCases = confirm ("Do you want lowercases in your password ")
-  var upperCases = confirm ("Do you want uppercases in your password")
-  var special = confirm("Do you want special characters in your password?");
-  var minimumCount = 0;
-}
-
-// for (let j = 9 j <== 128; j++) {
-//   console.log(j);
-}
